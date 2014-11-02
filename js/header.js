22bd48c7c38f0f0b7fa5b7a00197d604aa66d73b
Nathan Bailey <dev@dnbailey.org>
2014-11-02 08:41:24 -0500

    Add toggle menu for small screens

--- js/header.js
+++ js/header.js
@@ -8,4 +8,38 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-var cbpAnimatedHeader=function(){function e(){window.addEventListener("scroll",function(){o||(o=!0,setTimeout(n,250))},!1)}function n(){var e=t();e>=c?classie.add(r,"site-header-shrink"):classie.remove(r,"site-header-shrink"),o=!1}function t(){return window.pageYOffset||i.scrollTop}var i=document.documentElement,r=document.querySelector(".site-header"),o=!1,c=300;e()}();
+var cbpAnimatedHeader = function() {
+    function e() {
+        window.addEventListener("scroll", function() {
+            o || (o = !0, setTimeout(n, 250))
+        }, !1)
+    }
+
+    function n() {
+        var e = t();
+        e >= c ? classie.add(r, "site-header-shrink") : classie.remove(r, "site-header-shrink"), o = !1
+    }
+
+    function t() {
+        return window.pageYOffset || i.scrollTop
+    }
+    var i = document.documentElement,
+        r = document.querySelector(".site-header"),
+        o = !1,
+        c = 300;
+    e()
+}();
+
+/* Show Menu on Large Screens */
+$(window).on('load resize', function(){
+    if ($("#site-nav").css("float") == "right" ){
+        $('#site-nav').show();
+    }
+});
+
+$(document).ready(function() {    
+    /* Toggle the Menu on Small Screens */
+    $('#menu-toggle').click(function(){
+       $('#site-nav').toggle("slow","swing") 
+    });
+});
